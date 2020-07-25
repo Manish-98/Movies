aec77c1fba95672caa428ea8e2d5db32a46c9331
Manish-98 <manishlath3445@gmail.com>
2020-07-25 23:00:03 +0530

    Initial Commit. SpringBoot Initialised

---
+++ src/main/kotlin/com/project/restApiSpringBoot_Movies/RestApiSpringBootMoviesApplication.kt
@@ -0,0 +1,11 @@
+package com.project.restApiSpringBoot_Movies
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestApiSpringBootMoviesApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestApiSpringBootMoviesApplication>(*args)
+}
---
+++ src/test/kotlin/com/project/restApiSpringBoot_Movies/RestApiSpringBootMoviesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.project.restApiSpringBoot_Movies
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestApiSpringBootMoviesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
